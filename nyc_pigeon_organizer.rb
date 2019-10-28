0f7f3f73f4befab4a420ab68cffa2befa4f9ec8b
J <jmrumingan@gmail.com>
2019-10-28 18:21:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+
+  new_hash = {}
+  new_hash2 = {}
+  
+  #dig through the layers, find the specific names
+  #make keys out of the names, then add the previous layers as key-value pairs 
+  
+  data.each do |key, value|
+    
+    value.each do |key2, value2|
+      
+      value2.each do |pigeon_name|
+        #binding.pry 
+        
+        tiny_hash = {key=>key2}
+        new_hash[pigeon_name] = {key=>key2}
+        new_hash2[pigeon_name][key] = key2
+        
+        #new_hash.merge!(pigeon_name=>{key=>key2}) #no need to interpolate, already string 
+        
+      end 
+      #binding.pry 
+    end 
+  end 
+  puts "Start report \n #{new_hash} \n Hash2 #{new_hash2} \n *End report*" 
+  return new_hash 
 end
